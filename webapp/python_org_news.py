9f598f36f27c54be1f529c7f515dc35d2edb30e2
Rahmon22SoH <91434562+Rahmon22SoH@users.noreply.github.com>
2022-06-26 19:28:37 +0300

    creat db

--- webapp/python_org_news.py
+++ webapp/python_org_news.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
-
+from datetime import datetime
+from webapp.model import db, News
 
 def get_html(url):
     try:
@@ -10,7 +11,6 @@ def get_html(url):
     except(requests.RequestException, ValueError):
         print("Cетевая ошибка")
         return False
-
 
 def get_python_news():
     html = get_html("https://www.python.org/blogs/")
@@ -22,10 +22,16 @@ def get_python_news():
             title = news.find('a').text
             url = news.find('a')['href']
             published = news.find('time').text
-            result_news.append({
-                'title': title,
-                'url': url,
-                'published': published
-            })
-        return result_news
-    return False
+            try:
+                published = datetime.strptime(published, '%Y-%m-%d')
+            except ValueError:
+                published = datetime.now()
+            save_news(title, url, published)
+
+def save_news(title, url, published):
+    news_exists = News.query.filter(News.url == url).count()
+    print(news_exists)
+    if not news_exists:
+        news_news = News(title=title, url=url, published=published)
+        db.session.add(news_news)
+        db.session.commit()
