d9b61f2fdfb3de78315f429ec72b914aae8be4d5
Rahmon22SoH <91434562+Rahmon22SoH@users.noreply.github.com>
2022-06-28 09:26:43 +0300

    "new class user"

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 from webapp.weather import weather_by_city
 from webapp.model import db, News
-
+from webapp.forms import LoginForm
 
 def create_app():
     app = Flask(__name__)
@@ -14,5 +14,12 @@ def create_app():
         weather = weather_by_city(app.config['WEATHER_DEFAULT_CITY'])
         news_list = News.query.order_by(News.published.desc()).all()
         return render_template('index.html', page_title=title, weather=weather, news_list=news_list)
+
+    @app.route("/login")
+    def login():
+        title = 'Авторизация'
+        login_form = LoginForm()
+        return render_template('login.html', page_title=title, form=login_form)
+
 
     return app
--- webapp/model.py
+++ webapp/model.py
@@ -1,4 +1,6 @@
+from flask_login import UserMixin
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
 
@@ -12,3 +14,20 @@ class News(db.Model):
 
     def __repr__(self):
         return '<News {} {}>'.format(self.title, self.url)
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, nullable=True)
+    password = db.Column(db.String(128))
+    role = db.Column(db.String(10), index=True)
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+        return  check_password_hash((self.password, password))
+
+    def check_password(self, password):
+        return
+
+
+    def __repr__(self):
+        return '<User {} >'.format(self.username)
