c4faeeb1e5ad24af722864ee9adb37b9cc06fef4
Rahmon22SoH <mebelnazakazms@gmail.com>
2022-08-25 22:57:31 +0300

    add request.referrer + get_redirect_target

--- webapp/news/views.py
+++ webapp/news/views.py
@@ -1,6 +1,10 @@
-from flask import abort, Blueprint, current_app, render_template
-from webapp.news.model import News
+from flask import abort, Blueprint, current_app, flash, render_template, redirect, request
+from flask_login import current_user, login_required
+from webapp.db import db
+from webapp.news.model import Comment, News
 from webapp.weather import weather_by_city
+from webapp.news.forms import CommentForm
+from webapp.utils import get_redirect_target
 
 blueprint = Blueprint('news', __name__)
 
@@ -20,6 +24,25 @@ def single_news(news_id):
 
     if not my_news:
         abort(404)
-
+    comment_form = CommentForm(news_id=my_news.id)
     return render_template('news/single_news.html',
-                           page_title=my_news.title, news=my_news)
+                           page_title=my_news.title, news=my_news, comment_form=comment_form)
+
+
+@blueprint.route('/news/comment', methods = ['POST'])
+@login_required
+def add_comment():
+    form = CommentForm()
+    if form.validate_on_submit():
+        comment = Comment(text=form.comment_text.data, news_id=form.news_id.data, user_id=current_user.id)
+        db.session.add(comment)
+        db.session.commit()
+        flash('Комментарий успешно добавлен')
+    else:
+        for field, errors in form.errors.items():
+            for error in errors:
+                flash('Ошибка в заполнении поля "{}": - {}'.format(
+                    getattr(form, field).label.text,
+                    error
+                ))
+    return redirect(get_redirect_target())
--- webapp/user/views.py
+++ webapp/user/views.py
@@ -4,6 +4,7 @@ from flask_login import current_user, login_user, logout_user
 from webapp.db import db
 from webapp.user.forms import LoginForm, RegistrationForm
 from webapp.user.model import User
+from webapp.utils import get_redirect_target
 
 blueprint = Blueprint('user', __name__, url_prefix='/users')
 
@@ -11,7 +12,7 @@ blueprint = Blueprint('user', __name__, url_prefix='/users')
 @blueprint.route("/login")
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for("news.index"))
+        return redirect(get_redirect_target())
     title = 'Авторизация'
     login_form = LoginForm()
     return render_template('user/login.html', page_title=title, form=login_form)
@@ -20,12 +21,13 @@ def login():
 @blueprint.route('/process-login', methods=['POST'])
 def process_login():
     form = LoginForm()
+
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user and user.check_password(form.password.data):
             login_user(user, remember=form.remember_me.data)
             flash('Вы вошли на сайт')
-            return redirect(url_for('news.index'))
+            return redirect(get_redirect_target())
 
     flash('Неправильное имя пользователя или пароль')
     return redirect(url_for('user.login'))
